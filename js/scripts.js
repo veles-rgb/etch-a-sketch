4059887a6f9216e397f5971ff916aff50e5977e0
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-28 15:55:59 -0400

    Add reset button & change styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
 let gridRows = 16
 let gridColumns = gridRows
 let gridSize = gridRows * gridColumns;
-
 const gridContainer = document.querySelector(".grid-container");
 
 const createGridItem = function() {
     const newGridItem = document.createElement("div");
     newGridItem.classList.add("grid-item")
     gridContainer.appendChild(newGridItem)
-
     newGridItem.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = "grey";
+        e.target.style.backgroundColor = "lightgrey";
     })
 };
 
 for (let i = 0; i < gridSize; i++) {
     createGridItem()
 };
-
