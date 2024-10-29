41cd7c28ef185811d38a141ba611f669b167a1c8
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-29 15:18:06 -0400

    Re-write script to work with Set Grid Size btn
    Script now creates/resets grid when the btn is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
-let girdRows = 16;
-let gridColumns = girdRows;
-let girdSize = girdRows * gridColumns;
-let gridWidth = 810;
+const gridWidth = 810;
 const gridContainer = document.querySelector(".grid-container");
+const gridSizeBtn = document.querySelector(".grid-size-btn")
 
 const createGridItem = function() {
     const newGridItem = document.createElement("div");
     newGridItem.classList.add("grid-item");
-    newGridItem.style.width = gridWidth / girdRows + "px";
-    gridContainer.appendChild(newGridItem);
+    newGridItem.style.width = gridWidth / gridRows + "px";
+    newGridItem.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "red";
+    });
+    gridContainer.appendChild(newGridItem)
 };
 
-for (let i = 0; i < girdSize; i++) {
-    createGridItem();
+const createGrid = function(gridRows) {
+    for (let i = 0; i < (gridRows * gridRows); i++) {
+        createGridItem();
+    }
 };
+
+const deleteGrid = function() {
+    const gridItems = document.querySelectorAll(".grid-item");
+    gridItems.forEach((item) => {
+        item.remove();
+    });
+};
+
+gridSizeBtn.addEventListener("click", () => {
+    gridRows = prompt("Enter size");
+    if (gridRows < 16 || gridRows > 100) {
+        alert("Invalid Input. Choose between 16 & 100.");
+    } else {
+        deleteGrid();
+        createGrid(gridRows);
+    }
+});
