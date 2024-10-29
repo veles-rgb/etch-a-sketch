6f7a80788012da96e97ef36436baa12d2c007305
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-29 13:41:44 -0400

    Fix variables, function and for loop for grid size
    Fixed the createGridItem function to work no matter the grid size.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
-let gridRows = 16
-let gridColumns = gridRows
-let gridSize = gridRows * gridColumns;
+let girdRows = 16;
+let gridColumns = girdRows;
+let girdSize = girdRows * gridColumns;
+let gridWidth = 810;
 const gridContainer = document.querySelector(".grid-container");
 
 const createGridItem = function() {
     const newGridItem = document.createElement("div");
-    newGridItem.classList.add("grid-item")
-    gridContainer.appendChild(newGridItem)
-    newGridItem.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = "lightgrey";
-    })
+    newGridItem.classList.add("grid-item");
+    newGridItem.style.width = gridWidth / girdRows + "px";
+    gridContainer.appendChild(newGridItem);
 };
 
-for (let i = 0; i < gridSize; i++) {
-    createGridItem()
+for (let i = 0; i < girdSize; i++) {
+    createGridItem();
 };
