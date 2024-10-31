921c0dcdcb68ad97c373c3d658061724845dc5de
veles-rgb <181009546+veles-rgb@users.noreply.github.com>
2024-10-31 14:21:26 -0400

    Add functionality to Draw Mode buttons

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,11 @@ const gridSizeBtn = document.querySelector(".grid-size-btn");
 const clearBtn = document.querySelector(".clear-btn"); 
 const randBtn = document.querySelector(".rand-btn");
 const progBtn = document.querySelector(".prog-btn");
+const hoverBtn = document.querySelector(".hover-btn");
+const clickBtn = document.querySelector(".click-btn");
 
-let currentMode = "rand";
+let currentColorMode = "rand";
+let currentDrawMode = "mouseenter";
 
 // Random RGB color mode logic
 const getRandomColor = function(item) {
@@ -13,7 +16,7 @@ const getRandomColor = function(item) {
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     let randomRgb = `rgb(${r}, ${g}, ${b})`;
-    item.addEventListener("mouseenter", () => {
+    item.addEventListener(currentDrawMode, () => {
         item.style.backgroundColor = randomRgb;
     });
 };
@@ -21,7 +24,7 @@ const getRandomColor = function(item) {
 // Progressive color mode logic
 const getProgressiveColor = function(item) {
     let a = 0.1;
-    item.addEventListener("mouseenter", () => {
+    item.addEventListener(currentDrawMode, () => {
         a = Math.min(a + 0.10, 1.0);
         item.style.backgroundColor = `rgba(0, 0, 0, ${a})`;
     });
@@ -29,9 +32,9 @@ const getProgressiveColor = function(item) {
 
 // Determine which color mode to use
 const colorMode = function(item) {
-    if (currentMode === "rand") {
+    if (currentColorMode === "rand") {
         getRandomColor(item);
-    } else if (currentMode === "prog") {
+    } else if (currentColorMode === "prog") {
         getProgressiveColor(item);
     } else {
         alert("Error: no color mode selected");
@@ -79,14 +82,28 @@ clearBtn.addEventListener("click", () => {
 randBtn.addEventListener("click", () => {
     randBtn.classList.add("selected")
     progBtn.classList.remove("selected")
-    currentMode = "rand";
+    currentColorMode = "rand";
 });
 
 // Progressive darkness color mode button event listener
 progBtn.addEventListener("click", () => {
     progBtn.classList.add("selected");
     randBtn.classList.remove("selected");
-    currentMode = "prog";
+    currentColorMode = "prog";
+});
+
+// Hover draw mode click event listener
+hoverBtn.addEventListener("click", () => {
+    hoverBtn.classList.add("selected");
+    clickBtn.classList.remove("selected");
+    currentDrawMode = "mousedown";
+});
+
+// Click draw mode click event listener
+clickBtn.addEventListener("click", () => {
+    clickBtn.classList.add("selected");
+    hoverBtn.classList.remove("selected");
+    currentDrawMode = "click";
 });
 
 // Click event for grid size
